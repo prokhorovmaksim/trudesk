a7e93fb940bc0cb576f40f258dbed320026c9137
ProkhorovMax <prokhorovmaxim2009@yandex.ru>
2022-11-16 17:22:15 +0300

    site title

--- test/1_sessions.js
+++ test/1_sessions.js
@@ -39,7 +39,7 @@ describe('sessions', function () {
     agent.get('http://localhost:3111/').end(function (err, res) {
       expect(err).to.not.exist
       expect(res.status).to.equal(200)
-      expect(res.text).to.include('<title>Trudesk &middot; Dashboard</title>')
+      expect(res.text).to.include('<title>Time &middot; Dashboard</title>')
 
       done()
     })
@@ -49,7 +49,7 @@ describe('sessions', function () {
     unauthAgent.get('http://localhost:3111/tickets').end(function (err, res) {
       expect(err).to.not.exist
       expect(res.status).to.equal(200)
-      expect(res.text).to.include('<title>Trudesk &middot; Login</title>')
+      expect(res.text).to.include('<title>Time &middot; Login</title>')
 
       done()
     })
@@ -59,7 +59,7 @@ describe('sessions', function () {
     unauthAgent.get('http://localhost:3111/install').end(function (err, res) {
       expect(err).to.not.exist
       expect(res.status).to.equal(200)
-      expect(res.text).to.include('<title>Trudesk &middot; Login</title>')
+      expect(res.text).to.include('<title>Time &middot; Login</title>')
 
       done()
     })
