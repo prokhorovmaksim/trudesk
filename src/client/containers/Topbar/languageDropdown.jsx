f23cfc4ea4fd2209ea9f44cfe8bed4c70bf5a07c
ProkhorovMax <prokhorovmaxim2009@yandex.ru>
2022-11-18 21:02:11 +0300

    language and i18n

--- src/client/containers/Topbar/languageDropdown.jsx
+++ src/client/containers/Topbar/languageDropdown.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import { makeObservable } from 'mobx'
 
+import i18n from '../../i18n'
+
 import PDropdown from 'components/PDropdown'
 
 
@@ -12,6 +14,12 @@ class LanguagesDropdownPartial extends React.Component {
     super(props)
 
     makeObservable(this)
+  }
+
+  onLanguageChange(newLanguage) {
+    console.log('CLICKED ON:')
+    console.log(newLanguage)
+    i18n.changeLanguage(newLanguage);
   }
 
   render () {
@@ -30,12 +38,14 @@ class LanguagesDropdownPartial extends React.Component {
       >
         <div className={'ldrop-content'}>
           <div className={'language-drop-actions'}>
-            <div className={'action-change-language'}>
-              <img  src={'/img/flag_usa.png'} alt="flagUsa"/>
+            <div className={'action-change-language'}
+                 onClick={() => this.onLanguageChange("en")}>
+              <img src={'/img/flag_usa.png'} alt="flagUsa" />
               <span>English</span>
             </div>
-            <div className={'action-change-language'}>
-              <img  src={'/img/flag_rus.png'} alt="flagRus"/>
+            <div className={'action-change-language'}
+                 onClick={() => this.onLanguageChange("ru")}>
+              <img src={'/img/flag_rus.png'} alt="flagRus" />
               <span>Русский</span>
             </div>
           </div>
